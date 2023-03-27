02c09cc137853882efcb9af21dcad3e82676be45
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2023-03-27 21:25:00 +0100

    added animation to home page

--- src/pages/Home-page.vue
+++ src/pages/Home-page.vue
@@ -10,15 +10,22 @@
         </ul>
       </nav>  
     </header>
-    <main>
-      
+    <main class="container">
+        <p>Hello 👋 I'm </p>
+      <section class="animation">
+        <div class="first"><div>Chijioke Elijah,  A Web Developer</div></div>
+        <div class="second"><div>A technical writer</div></div>
+        <div class="third"><div>a student at altschool africa</div></div>
+      </section>
     </main>
-  </template>
-
-
+</template>
 
 
 <style scoped>
+main {
+  font-family:'Roboto';
+  background-color: #131417;  
+}
 nav {
   display: flex;
   flex-wrap: wrap;
@@ -54,14 +61,7 @@ ul li a {
   font-size: large;
   text-decoration: none;
 }
-p {
-  padding: 100px 0px;
-  margin: 10px;
-  font-size: larger;
-  font-weight: bold;
-  align-items: center;
-  position: relative;
-}
+
 h1 {
   position: absolute;
 }
@@ -73,6 +73,58 @@ button {
   border-color: aquamarine;
   background-color: whitesmoke;
   color: black;
+}
+p {
+  text-shadow: 0 0 7px rgba(255,255,255,.3), 0 0 3px rgba(255,255,255,.3);
+}
+.container {
+  color: #e5e5e5;
+  font-size: 2.26rem;
+  text-transform: uppercase;
+  /* height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center; */
+}
+
+.animation {
+  /* height:50px; */
+  overflow:hidden;
+  margin-left: 1rem;
+}
+
+.animation > div > div {
+  padding: 0.25rem 0.75rem;
+  height:2.81rem;
+  margin-bottom: 2.81rem;
+  display:inline-block;
+}
+
+.animation div:first-child {
+  animation: text-animation 8s infinite;
+}
+
+.first div {
+  background-color:#20a7d8;
+}
+.second div {
+  background-color:#CD921E;
+}
+.third div {
+  background-color:#c10528;
+}
+
+@keyframes text-animation {
+  0% {margin-top: 0;}
+  10% {margin-top: 0;}
+  20% {margin-top: -5.62rem;}
+  30% {margin-top: -5.62rem;}
+  40% {margin-top: -11.24rem;}
+  60% {margin-top: -11.24rem;}
+  70% {margin-top: -5.62rem;}
+  80% {margin-top: -5.62rem;}
+  90% {margin-top: 0;}
+  /* 100% {margin-top: 0;} */
 }
 </style>
 
